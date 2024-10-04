890e4ed3ecb1a45cfd7048e3a64969dd658d1ff6
JulienGAMartin <julien.martin@uottawa.ca>
2024-10-03 21:35:56 -0400

    adding no render option

--- utils/_render-publish.sh
+++ utils/_render-publish.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
-if [[ $# -eq 0 ]] || [ $1 != "--no-render" ]; then
+if [[ $# -eq 0 ]]; then
+render=true
+else
+if [ $1 != "--no-render" ]; then
+  echo "argument not recognized"
+  exit 1
+else
+render=$1
+fi
+fi
+
+if [ $render != "--no-render" ]; then
   echo Rendering
-
   quarto render
-
 if [ $? -ne 0 ]; then
   echo "render command failed"
   exit 1
